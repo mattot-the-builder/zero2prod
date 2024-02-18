36237d0f706a408e7a68e10bcbc127f7fb3c25a3
mattot-the-builder <mattot.builder@gmail.com>
2024-02-18 19:14:12 +0800

    Finished dividing lib.rs into modules

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,6 +1,6 @@
 use std::net::TcpListener;
 
-use zero2prod::run;
+use zero2prod::startup::run;
 
 fn spawn_app() -> String {
     let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
