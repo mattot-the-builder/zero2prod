1bd178d0ccfbee6cd3deefde4991e8ecdf81b311
mattot-the-builder <mattot.builder@gmail.com>
2024-02-18 21:08:08 +0800

    Finished chapter 3

--- src/startup.rs
+++ src/startup.rs
@@ -1,14 +1,21 @@
 use std::net::TcpListener;
 
 use actix_web::{dev::Server, web, App, HttpServer};
+use sqlx::PgPool;
 
 use crate::routes::{health_check, subscribe};
 
-pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(|| {
+pub fn run(listener: TcpListener, db_pool: PgPool) -> Result<Server, std::io::Error> {
+    // wrap connection in smart pointer
+    let db_pool = web::Data::new(db_pool);
+
+    // capture connection from surrounding environment
+    let server = HttpServer::new(move || {
         App::new()
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
+            // register connection as part of application state
+            .app_data(db_pool.clone())
     })
     .listen(listener)?
     .run();
